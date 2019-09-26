3aa1a0abec7f7ebf9d9c92c193ef326298075311
Brett Hayes <bretthayes@live.ca>
2019-09-25 22:09:44 -0400

    Initial project release

---
+++ example/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Button } from '../src';
+
+ReactDOM.render(
+  <Button>My Custom Button</Button>,
+  document.getElementById('root')
+);
